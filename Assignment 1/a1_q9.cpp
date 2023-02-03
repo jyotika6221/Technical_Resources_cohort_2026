329e2cb716e7bab6f70f035fa45a0f4c97a0c9bb
Jyotika <83125363+jyotika6221@users.noreply.github.com>
2023-02-04 01:09:00 +0530

    Submitting question 9 of assignment 1

---
+++ Assignment 1/a1_q9.cpp
@@ -0,0 +1,28 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    //fimding the gcd of two numbers
+    int num1,num2,gcd=1,lcm=1;
+    cout<<"enter two numbers:";
+    cin>>num1>>num2;
+    int max=num1;
+    if(num2>max){
+        max=num2;
+
+    }
+    
+    for(int i=1;i<max;i++){
+        if(num1%i==0 && num2%i==0){
+            gcd=i;
+
+        }
+
+
+    }
+    lcm=num1*num2/gcd;
+    cout<<"gcd of a and b is:"<<gcd<<endl;
+    cout<<"lcm of a and b is:"<<lcm;
+return 0;
+
+}
