beaad0229dfd5fe23d0e5d90145d9dbeb1f8da90
Jyotika <83125363+jyotika6221@users.noreply.github.com>
2023-02-07 19:55:36 +0530

    Submitting question 10 of assignment 2

--- Assignment 2/a2_q10.cpp
+++ Assignment 2/a2_q10.cpp
@@ -1,1 +1,32 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char **argv){
+    int n;
+    cin >> n;
+    int space=n/2;
+    int star=1;
+ 
+    for(int i=1;i<=n;i++){
+        for(int j=1;j<=space;j++){
+            cout<<"\t";
+        }
+        for(int k=1;k<=star;k++){
+            if(k==1 || k==star){
+                cout<<"*";
+            }
+            cout<<"\t";
+        }
+        if(i<=n/2){
+            space-=1;
+            star+=2;
+        }
+        else{
+            space+=1;
+            star-=2;
+        }
+        cout<<endl;
+    }
+    return 0;
+
+}
