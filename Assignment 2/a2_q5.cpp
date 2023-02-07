dd5f59e324d81ee56a926c81cdea67bd3f66648a
Jyotika <83125363+jyotika6221@users.noreply.github.com>
2023-02-07 19:34:26 +0530

    Submitting question 5 of assignment 2

--- Assignment 2/a2_q5.cpp
+++ Assignment 2/a2_q5.cpp
@@ -1,1 +1,35 @@
+#include <iostream>
+using namespace std;
 
+int main(int argc, char **argv)
+{
+    int n;
+    cin >> n;
+    int space,str,i;
+    space= n/2;
+    str=1;
+    for(i=1;i<=n;i++)
+    {
+        for(int j=1;j<=space;j++)
+        {
+            cout<<"\t";
+        }
+        for(int j=1;j<=str;j++)
+        {
+            cout<<"*\t";
+
+        }
+        if (i<=n/2)
+        {
+            str+=2;
+            space--;
+        }
+        else
+        {
+            space++;
+            str-=2;
+        }
+        cout<<"\n";
+    }
+return 0;
+}
